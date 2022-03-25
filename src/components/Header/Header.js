a02ba5d7e9f53ce74659d569e5581f0f9eb6fb64
abukawsar47 <abukawsar47ak@gmail.com>
2022-03-25 23:12:25 +0600

    'fakeData'

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <h1>MRF SPORTS</h1>
+            <h1>CRICKET SPORTS</h1>
         </div>
     );
 };
